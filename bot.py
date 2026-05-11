ae28c65905fc7f0b4d45be0a5b596fee38d1f237
Altaf8099 <altaf80993@gmail.com>
2026-05-11 15:32:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import re
 import os
 import logging
+from flask import Flask
 from dotenv import load_dotenv
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
@@ -10,8 +11,10 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 GROUP_ID = os.getenv("GROUP_ID")
+PORT = int(os.getenv("PORT", 10000))
 
 logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 def is_valid(text):
     pattern = re.compile(
@@ -41,10 +44,19 @@ async def filter_msg(update: Update, context: ContextTypes.DEFAULT_TYPE):
         except:
             pass
 
+flask_app = Flask(__name__)
+
+@flask_app.route("/")
+def home():
+    return "Bot Running"
+
 def main():
     app = Application.builder().token(BOT_TOKEN).build()
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, filter_msg))
     app.run_polling()
+    logger.info("Bot polling started...")
 
 if __name__ == "__main__":
-    main()
+    import threading
+    threading.Thread(target=main).start()
+    flask_app.run(host="0.0.0.0", port=PORT)
