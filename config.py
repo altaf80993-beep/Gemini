add2306bd38883c25bafde0c6743ae01e4138ff0
Altaf8099 <altaf80993@gmail.com>
2026-05-24 20:29:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,23 +4,4 @@
 
 # Admin Configuration
 ADMIN_ID = 6722137021
-ADMIN_USERNAME = "@crypto_8099"
-
-# Bot Configuration
-BOT_TOKEN = "your_bot_token_here"
-GROUP_ID = "-1003974490347"
-PORT = 10000
-
-# Escrow Rooms
-ROOMS = [
-    {
-        "room_num": 1,
-        "room_id": -1003974490347,
-        "busy": False
-    },
-    {
-        "room_num": 2,
-        "room_id": -1003766531525,
-        "busy": False
-    },
-]
+ADMIN_USERNAME = "@crypto_8099
