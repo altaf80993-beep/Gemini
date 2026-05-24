9d29311badf55ffe89c93d8f2cfb5c95087d9ce3
Altaf8099 <altaf80993@gmail.com>
2026-05-24 20:55:11 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,26 @@
+# ======================
+# BOT CONFIGURATION (REAL)
+# ======================
+
+# Admin Configuration
+ADMIN_ID = 6722137021
+ADMIN_USERNAME = "@crypto_8099"
+
+# Bot Configuration - REAL TOKEN
+BOT_TOKEN = "8689223129:AAE_39NLwUrVx9n5PXcG4b6c1TQTkhlRhDc"
+GROUP_ID = "-1003974490347"
+PORT = 10000
+
+# Escrow Rooms
+ROOMS = [
+    {
+        "room_num": 1,
+        "room_id": -1003974490347,
+        "busy": False
+    },
+    {
+        "room_num": 2,
+        "room_id": -1003766531525,
+        "busy": False
+    },
+]
